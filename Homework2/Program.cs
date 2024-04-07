024242ee46d3d4d6006fa1f12664626a38f72155
Yuri Lagoda <epicbeaver33@gmail.com>
2024-04-07 22:58:23 +0300

    Homework is complete

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -4,15 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
-            Console.ReadKey();
-        }
+        Console.OutputEncoding = Encoding.Unicode;
+        // Обчислення величини знижки
+        double price = 15.99; // приклад значення вартості товару
+        int discountPercentage = 10; // приклад значення відсотка знижки
+
+        double discount = (discountPercentage / 100) * price;
+
+        Console.WriteLine("Сума знижки:" + discount);
+        Console.WriteLine("                         ");
+
+
+        // Час в секундах
+        int hours = 2; // приклад значення годин
+        int minutes = 30; // приклад значення хвилин
+        int seconds = 45; // приклад значення секунд
+
+        int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+        Console.WriteLine("Загальний час у секундах:" + totalSeconds);
+        Console.WriteLine("                         ");
+
+
+        // Інформація про книгу
+        string назва = "Відьмак";
+        string автор = "Анджей Сапковський";
+        int рік = 1993;
+        double ціна = 25.99;
+
+        // Виводимо інформацію про книгу у форматованому рядку
+        Console.WriteLine("Інформація про книгу:");
+        Console.WriteLine("Назва: {0}", назва);
+        Console.WriteLine("Автор: {0}", автор);
+        Console.WriteLine("Рік: {0}", рік);
+        Console.WriteLine("Ціна: ${0}", ціна);
     }
 }
